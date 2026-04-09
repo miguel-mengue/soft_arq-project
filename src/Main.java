3c3c5d70551f72154537c686a24d3610f253869a
miguel-mengue <miguelmengue123@gmail.com>
2026-04-09 19:17:29 -0300

    tests commit

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,15 @@ void main() {
     product.setPrice(new BigDecimal("1000"));
     product.setPrice(new BigDecimal("999"));
 
+    product.setName("Tablet");
+    product.setSku("SAM-TAB-S9-11-256GB-P");
+    product.setPrice(new BigDecimal("1500"));
+    product.setPrice(new BigDecimal("1499"));
+
+    product.setName("Notebook");
+    product.setSku("NOTE-DELL-15-500GB-AZUL");
+    product.setPrice(new BigDecimal("2000"));
+    product.setPrice(new BigDecimal("1900"));
+
     System.out.println(product.toString());
 }
